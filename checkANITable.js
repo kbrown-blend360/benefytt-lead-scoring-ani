e44c657887e8cd7085e12e2885cd75a7f2cd838d
kbrown-blend360 <58896465+kbrown-blend360@users.noreply.github.com>
2021-10-08 15:24:25 -0400

    Add files via upload

---
+++ checkANITable.js
@@ -0,0 +1,45 @@
+const AWS = require("aws-sdk");
+
+AWS.config.update({
+  region: "us-east-1",
+  endpoint: "https://dynamodb.us-east-1.amazonaws.com"
+});
+
+const getItem = async (event) => {
+  //default success
+  var message = "success"
+
+  var params = {
+    TableName: "ani",
+    Key:{
+      "ani": event.ani,
+    }
+  }
+  
+  let item;
+
+  var docClient = new AWS.DynamoDB.DocumentClient();
+  
+  await docClient.get(params, function(err, data) {
+    if (err) {
+      console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2))
+      message = "error"
+    } else {
+      //success retrieval
+
+      console.log("GetItem succeeded:", JSON.stringify(data, null, 2))
+      item = data
+      brand = data.brand // togetherhealth or go health
+    }
+  })
+
+  return { 
+    message,
+    item,
+    event
+  }
+
+};
+
+
+module.exports = getItem;
